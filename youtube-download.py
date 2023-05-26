0740968f1d25530a3c890e4fe5803f33087a38ba
ClapperCoder96 <133124009+ClapperCoder96@users.noreply.github.com>
2023-05-26 14:50:36 -0400

    Create youtube-download.py
    You can download a YouTube video to watch or to listen to ad-free!

---
+++ youtube-download.py
@@ -0,0 +1,20 @@
+from pytube import YouTube 
+import os 
+
+#enter mp3 or mp4 for this
+choose = input("Are you downloading this video or coverting it to audio? ").lower()
+
+if choose == "mp4":
+    link = input("Enter your link: ")
+    yt = YouTube(link)
+    yt.streams.get_highest_resolution().download()
+    print("Download Succsessful")
+ 
+elif choose == "mp3":
+    link = input("Enter the link you want to download: ")
+    yt = YouTube(link)
+    whenyoudontknowwhattonameurvar = yt.streams.filter(only_audio=True).get_audio_only()
+    whenyoudontknowwhattonameurvar.download()
+    print("Download Succsessful:")
+else: 
+    print("Please complete the question with either mp4(video) or mp3(audio)")
