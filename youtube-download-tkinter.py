930f2eb48f94fa8ecb6f8770dff984df66bb0916
ClapperCoder96 <133124009+ClapperCoder96@users.noreply.github.com>
2023-05-27 18:37:14 -0400

    Update youtube-download-tkinter.py

--- youtube-download-tkinter.py
+++ youtube-download-tkinter.py
@@ -1,5 +1,4 @@
-#A much more advanced version of my YouTube downloader which is more user friendly and is in tkinter
-#STILL IN WORK#
+#A much more advanced version of my YouTube downloader which is more user friendly and is in tkinter 
 
 import tkinter as tk
 from tkinter import *
@@ -10,38 +9,44 @@ window = Tk()
 canvas = Canvas(window, width=300, height=200)
 canvas.pack()
 
-choose = input("Would you like to download or convert to audio: ").lower()
-if choose == "download":
-    l = Label(canvas, text="Please input your video link that you would like to download")
-    l.config(font =("Courier", 14))
-    l.pack()
-    def download():
-        
-        global e
-        string = e.get() 
-        yt = YouTube(string)
-        yt.streams.get_highest_resolution().download()
-        print("Download Succsessful")
-    e = Entry(canvas)
-    e.pack()
-    e.focus_set()
-elif choose == "audio":
-    l = Label(canvas, text="Please input your video link that you would like to convert")
-    l.config(font =("Courier", 14))
-    l.pack()
-    def audio():
-        global e 
-        string = e.get()
-        yt = YouTube(string)
-        whenyoudontknowwhattonameurvar = yt.streams.filter(only_audio=True).get_audio_only()
-        whenyoudontknowwhattonameurvar.download()
-        print("Download Succsessful:")
-    e = Entry(canvas)
-    e.pack()
-    e.focus_set()
+l = Label(canvas, text="Would you like to download or convert to audio: ")
+l.pack()
+entry = tk.Entry()
+entry.pack()
 
-b = Button(canvas,text='okay',command=choose)
-b.pack(side='bottom') 
-
-
+def on_button():
+    if entry.get() == "download":
+        l = Label(canvas, text="Please input your video link that you would like to download")
+        l.config(font =("Courier", 14))
+        l.pack()
+        def download(): 
+            #fix this:
+            global e
+            string = e.get() 
+            yt = YouTube(string)
+            yt.streams.get_highest_resolution().download()
+            print("Download Succsessful")
+        e = Entry(canvas)
+        e.pack()
+        e.focus_set()
+        b = Button(canvas,text='okay',command=download)
+        b.pack(side='bottom') 
+    elif entry.get() == "audio":
+        l = Label(canvas, text="Please input your video link that you would like to convert")
+        l.config(font =("Courier", 14))
+        l.pack()
+        def audio():
+            global e 
+            string = e.get()
+            yt = YouTube(string)
+            whenyoudontknowwhattonameurvar = yt.streams.filter(only_audio=True).get_audio_only()
+            whenyoudontknowwhattonameurvar.download()
+            print("Download Succsessful:")
+        e = Entry(canvas)
+        e.pack()
+        e.focus_set()
+        b = Button(canvas,text='okay',command=audio)
+        b.pack(side='bottom') 
+button = tk.Button(canvas, text="Enter", command=on_button)
+button.pack()
 tk.mainloop()
