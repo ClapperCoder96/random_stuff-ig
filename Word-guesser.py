61b32238299ab9619b93c03d3bb7ca2cde3217af
ClapperCoder96 <133124009+ClapperCoder96@users.noreply.github.com>
2023-06-04 13:48:56 -0400

    Update Word-guesser.py

--- Word-guesser.py
+++ Word-guesser.py
@@ -1,45 +1,54 @@
-#This program is a basic word guesser for practicing slicing and higher level functions 
-
 import random
+import sys
 
-words = ['hippo', 'jam', 'jelly', 'not', 'display', 'imagine', 'coder', 'programmer', 'python', 'bozo', 'watch']
+def choose_word():
+    #HAHAHAHHAHAHAHHAH SO FUNNY I KNOW YOU ARE LAUGHING RIGHT NOW!!! XDXDXDDXDXDXDXDXDDXDXDXDXDXDDXDXDXDXDXDXD 
+    words = ["balls", "github", "balls", "nuts"]
+    word = random.choice(words).lower()
+    return word
 
-word = random.choice(words)
 
-spaces = ['*']* len(word)
-
-def get_letter_position(guess, word, spaces):
-    index = -2
-    while index != -1:
-        if guess in word:
-            index = word.find(guess)
-            removed_character ='*'
-            word = word[:index]+removed_character+word[index+1:]
-            spaces[index] = guess
+def word_in_progress(word, guesses):
+    word_in_progress = ""
+    for letter in word:
+        if letter in guesses:
+            word_in_progress += letter
         else:
-            index = -1
-     
-    return (word, spaces)
+            word_in_progress += "*"
+    return word_in_progress
 
 
-def win_check():
-    for i in range(0, len(spaces)):
-        if spaces[i] == '*':
-            return -1
-     
-    return 1
+def main(word):
+    lettersguessed = []
+    chances = 10
+    print("You are looking for a word that is " + str(len(word)) + " letters long.")
 
-num_turns = 10
-for i in range(-1, num_turns):
-    guess = input('Guess a character:')
- 
-    if guess in word:
-        word, spaces = get_letter_position(guess, word, spaces)
-        print(spaces)
-    else:
-        print("Wrong")
-    if win_check() == 1:
-        print('you won')
-        break
-     
-    print('you have '+str(num_turns - i)+' turns left.')
+    while True:
+        if chances != 0:
+            print("\nYou have " + str(chances) + " chances left.")
+            print("Words so far " + word_in_progress(word, lettersguessed))
+            print("Letters guessed: " + str(lettersguessed))
+            guess = input("Guess: ").lower()[0]
+
+            if guess not in lettersguessed:
+                lettersguessed.append(guess)
+
+            if word_in_progress(word, lettersguessed) == word:
+                print("\nCongratulations! You got the right word: " + word)
+                break
+
+            else:
+                chances -= 1
+                if guess in word:
+                    print("Correct letter!")
+                else:
+                    print(guess + " is not in the word.")
+        else:
+            print("\nOops you ran out of guesses. The correct word was " + word)
+            break
+
+
+while True:
+    word = choose_word()
+    main(word)
+    sys.exit()
